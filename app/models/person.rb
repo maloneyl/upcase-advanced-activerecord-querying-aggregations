808a4f76327699cc69a6994c6861f5fed2feea08
Maloney L <maloneyl@users.noreply.github.com>
2019-02-11 18:35:20 +0000

    add examples (+ tests) from tutorial

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,6 +4,56 @@ class Person < ActiveRecord::Base
   belongs_to :manager, class_name: "Person", foreign_key: :manager_id
   has_many :employees, class_name: "Person", foreign_key: :manager_id
 
+  def self.average_salary
+    average(:salary)
+  end
+
+  def self.non_billable_salaries
+    joins(:role).
+    where(roles: { billable: false }).
+    sum(:salary)
+  end
+
+  def self.average_salary_by_role
+    joins(:role).
+    group("roles.name").
+    average(:salary)
+  end
+
+  def self.employee_count
+    joins(
+      "LEFT JOIN people employees ON employees.manager_id = people.id"
+    ).
+    group("people.name").
+    count("employees.id")
+  end
+
+  def self.with_lower_than_average_salaries_at_location
+    joins(
+      "INNER JOIN (" +
+        self.
+          select("location_id, AVG(salary) as average").
+          group("location_id").
+          to_sql +
+      ") salaries " \
+      "ON salaries.location_id = people.location_id"
+    ).
+    where("people.salary < salaries.average")
+  end
+
+  def self.highest_salaried_ordered_by_name
+    joins(
+      "INNER JOIN (" +
+        self.
+          select("id, rank() OVER (ORDER BY salary DESC)").
+          to_sql +
+      ") salaries " \
+      "ON salaries.id = people.id"
+    ).
+    where("salaries.rank <= 3").
+    order(:name)
+  end
+
   def self.maximum_salary_by_location
     group(:location_id).maximum(:salary)
   end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,6 +1,90 @@
 require "spec_helper"
 
 describe Person do
+  describe ".average_salary" do
+    it "finds the average salary across all people" do
+      create(:person, salary: 40_000)
+      create(:person, salary: 30_000)
+
+      expect(Person.average_salary).to eq(35_000)
+    end
+  end
+
+  describe ".non_billable_salaries" do
+    it "finds the salaries of all people in non-billable roles" do
+      create(:person, role: create(:role, billable: true), salary: 10_000)
+      create(:person, role: create(:role, billable: false), salary: 20_000)
+      create(:person, role: create(:role, billable: true), salary: 30_000)
+      create(:person, role: create(:role, billable: false), salary: 40_000)
+
+      result = Person.non_billable_salaries
+
+      expect(result).to eq(60_000)
+    end
+  end
+
+  describe ".average_salary_by_role" do
+    it "finds the average salary by role" do
+      create(:person, role: create(:role, name: "Researcher"), salary: 40_000)
+      create(:person, role: create(:role, name: "Researcher"), salary: 50_000)
+      create(:person, role: create(:role, name: "Designer"), salary: 55_000)
+      create(:person, role: create(:role, name: "Designer"), salary: 45_000)
+
+      result = Person.average_salary_by_role
+
+      expect(result).to eq(
+        "Designer" => 50_000,
+        "Researcher" => 45_000
+      )
+    end
+  end
+
+  describe ".employee_count" do
+    it "counts employees for each person, including those who have no employees" do
+      manager_a = create(:person, name: "Manager A", manager: nil)
+      employee_a = create(:person, name: "Employee A", manager: manager_a)
+      manager_b = create(:person, name: "Manager B", manager: nil)
+      employee_b1 = create(:person, name: "Employee B1", manager: manager_b)
+      employee_b2 = create(:person, name: "Employee B2", manager: manager_b)
+
+      result = Person.employee_count
+
+      expect(result).to eq(
+        "Employee A" => 0,
+        "Employee B1" => 0,
+        "Employee B2" => 0,
+        "Manager A" => 1,
+        "Manager B" => 2
+      )
+    end
+  end
+
+  describe ".with_lower_than_average_salaries_at_location" do
+    it "finds people with lower than average salaries at their location" do
+      location = create(:location)
+      create(:person, location: location, salary: 40_000)
+      create(:person, location: location, salary: 35_000)
+      low_paid_person = create(:person, location: location, salary: 20_000)
+
+      result = Person.with_lower_than_average_salaries_at_location
+
+      expect(result).to eq([low_paid_person])
+    end
+  end
+
+  describe ".highest_salaried_ordered_by_name" do
+    it "finds the highest salaried people, ordered by name" do
+      top_earner = create(:person, name: "One", salary: 100_000)
+      second_highest_earner = create(:person, name: "Two", salary: 80_000)
+      third_highest_earner = create(:person, name: "Three", salary: 75_000)
+      normal_earner = create(:person, name: "Nope", salary: 50_000)
+
+      result = Person.highest_salaried_ordered_by_name
+
+      expect(result.map(&:name)).to eq(%w(One Three Two))
+    end
+  end
+
   describe ".maximum_salary_by_location" do
     it "finds the highest salary at each location" do
       [50_000, 60_000].each do |highest_salary|
